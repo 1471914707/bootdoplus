2c37512d944d9100e479883969cfebeb9e648158
1471914707@qq.com <1471914707@qq.com>
2019-05-12 00:26:34 +0800

    the frist commit

---
+++ src/main/resources/static/js/Api.js
@@ -0,0 +1,47 @@
+var BlogBaseUrl = "/blog";
+var BaseUrl = "/test";
+
+var Api = {
+    BlogOpenList: BlogBaseUrl + "/open/list",
+    UserGet: BaseUrl + "/getUser",
+
+    get:function(url,data,successFunction){
+        $.ajax({
+            url:url,
+            dataType:"json",
+            data:data,
+            type:"GET",
+            success:successFunction
+        })
+    },
+    get:function(url,data,successFunction,errorFunction){
+        $.ajax({
+            url:url,
+            dataType:"json",
+            data:data,
+            type:"GET",
+            success:successFunction,
+            error:errorFunction
+        })
+    },
+
+    post:function(url,data,successFunction){
+        $.ajax({
+            url:url,
+            dataType:"json",
+            data:data,
+            type:"POST",
+            success:successFunction
+        })
+    },
+    post:function(url,data,successFunction,errorFunction){
+        $.ajax({
+            url:url,
+            dataType:"json",
+            data:data,
+            type:"POST",
+            success:successFunction,
+            error:errorFunction
+        })
+    },
+}
